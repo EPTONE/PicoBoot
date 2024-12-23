6c2c42f60d7962a6427c62996f646a2933175a0f
EPTONE <fergusonchristopher363@gmail.com>
2024-12-23 15:58:38 -0500

    finally removed them

--- build/_deps/picotool-build/data_locs.cpp
+++
@@ -1,7 +0,0 @@
-
-#include <vector>
-#include <string>
-
-std::vector<std::string> data_locs = {
-    "./","/home/christopher/Programs/PERSONAL/Experiments/C/Pico/Bootloader/build/_deps/picotool/"
-};
