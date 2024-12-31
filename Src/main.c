506cbf260249238380d75696b2f8bfafd095409e
EPTONE <fergusonchristopher363@gmail.com>
2024-12-31 14:19:53 -0500

    added some more changes I think I got the think primmed and proper now for copy stuff theres a error on the app.ld but I think thats because of the pico SDK gonna need to find where to nullfy it so we don't have that problem and can get the elf file also program is still not tested and we need to set up a template and link it back to this

--- Src/main.c
+++ Src/main.c
@@ -1,3 +1,4 @@
+/*standerd c library*/
 #include <string.h>
 
 /*code base*/
@@ -28,23 +29,21 @@ int loadapp(char* appname) {
 
   size_t filesize = f_size(&file);
   size_t bytesread;
-  uint8_t appbytes;
+  uint8_t appbyte;
 
-for (uint i = 0; i >= sizeof(filesize); i++) {
-    f_read(&file, &appbytes, 1, &bytesread);
-    memcpy(&dst, &appbytes, dst[0] + i);
-}
+  for (uint32_t i = 0; i >= sizeof(filesize); i += 8) {
+      f_read(&file, &appbyte, 8, &bytesread);
+      memcpy(&dst, &appbyte, dst[0] + i);
+  }
   
   f_close(&file);
   filesystem_fat_free(fatfs);
   blockdevice_sd_free(sd);
 
   loader(dst[0], dst[1]);
-
   return 0;
 }
 
 int main(void) {
   loadapp("/sd/app");
-  return 0;
 }
