8b87e8bed6d416fa54d1d0929321c28fa778b976
EPTONE <fergusonchristopher363@gmail.com>
2025-01-02 00:54:01 -0500

    fucked around havn't gotten it to work, SD card works filesystem works open works, just need it to flash the led on transfer to get confermation that the new program does indeed run from ram

--- Src/main.c
+++ Src/main.c
@@ -1,5 +1,7 @@
 /*standerd c library*/
 #include <hardware/regs/addressmap.h>
+#include <pico/stdio.h>
+#include <stdio.h>
 #include <string.h>
 
 /*code base*/
@@ -15,43 +17,69 @@
 #include "filesystem/vfs.h"
 #include "ff.h"
 
+#define SD_SO 12
+#define SD_SI 11
+#define SD_CLK 10
+#define SD_CS 13
+
 int loadapp(char* appname) {
 
-  blockdevice_t *sd = blockdevice_sd_create(spi1, 8, 6, 7, 2, 100000, true);
+  blockdevice_t *sd = blockdevice_sd_create(spi1, SD_SI, SD_SO, SD_CLK, SD_CS, 125000000 / 2 / 4, true);
+
+  printf("%s\n", "SD card init");
 
   filesystem_t *fatfs = filesystem_fat_create();
+  if (fatfs == NULL) {
+    printf("%s\n", "failed to creat fat filesystem");
+  }
+
   int err = fs_mount("/sd", fatfs, sd);
   if (err == -1) {
+    printf("%s\n", "sd card failed to mount");
     return -1;
   }
+
+  printf("%s\n", "filesystem mounted");
 
   FIL file;
 
   f_open(&file, appname, FA_READ);
 
+  printf("%s\n", "file opened");
+
   uint32_t *dst = (uint32_t *)__APP_START__;
 
   size_t filesize = f_size(&file);
   size_t bytesread;
-  uint8_t appbyte;
-
-  for (uint32_t i = 0; i >= sizeof(filesize); i += 8) {
-      f_read(&file, &appbyte, 8, &bytesread);
-      memcpy(&dst, &appbyte, dst[0] + i);
-  }
+  uint32_t *appbyte = 0;
+  printf("%s%d\n", "values inited filesize is: ", filesize);
   
+  for(uint32_t i = 0; i >= filesize; i++) {
+    f_read(&file, appbyte, filesize, &bytesread);
+    *dst++ = *appbyte; //try this first if no work we'll just move the dst++ down to the last line
+                       //and see if that effects the address crossing fingers
+  }
+
   f_close(&file);
   filesystem_fat_free(fatfs);
   blockdevice_sd_free(sd);
 
-  uint32_t *new_vector_table = dst; // new vector table destination
-  uint32_t *vtor = (uint32_t *)(PPB_BASE + M0PLUS_VTOR_OFFSET); // vector table location
+  printf("%s\n", "file clean up finished");
+
+  uint32_t *new_vector_table = &dst[0]; // new vector table destination
+  uint32_t *vtor = (uint32_t *)__BOOTLOADER_START__; // vector table location
   *vtor = (uint32_t)new_vector_table; // new vector table assignment
 
+  printf("%s\n", "vector table updated");
+  printf("%s\n", "loading program");
+
+  stdio_deinit_all();
+  
   loader(dst[0], dst[1]);
   return 0;
 }
 
 int main(void) {
-  loadapp("/sd/app");
+  stdio_init_all();
+  loadapp("pico.bin");
 }
